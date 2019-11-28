6f1194c9961820a23fd22f8a86023c5974a7bd49
paweljumper20 <paweljumper20@gmail.com>
2019-11-28 18:47:00 +0100

    Pierwszy commit

---
+++ src/com/sda/kolejka/Main.java
@@ -0,0 +1,24 @@
+package com.sda.kolejka;
+
+public class Main {
+    public static void main(String[] args) {
+
+      KolejkaLista kolejka = new KolejkaLista();
+      kolejka.add(8);
+      kolejka.add(10);
+      kolejka.add(12);
+
+        System.out.println(kolejka);
+        int poczatek = kolejka.pop();
+        System.out.println(poczatek);
+        System.out.println(kolejka);
+        int pierwszyWKolejce = kolejka.push();
+        System.out.println(kolejka);
+
+        kolejka.add(2);
+        kolejka.add(56);
+        kolejka.add(67);
+        System.out.println(kolejka);
+
+    }
+}
